8510ee8f5c62e0c9440da28d78a87734b987d1ba
Hju Kneyck Flores <hjucode@gmail.com>
2024-06-07 07:09:47 +0800

    Refactor & impl. aggregation logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,161 @@
 wit_bindgen::generate!();
 
-use exports::jabratech::component_agg_ticker::agg_ticker::{Guest, AggregatedTickers};
-use wasi::logging::logging::*;
-use wasi::random::random::*;
-use wasmcloud::messaging::*;
-use serde::ser::{ Serializer, SerializeStruct};
+use std::vec::Vec;
+
+use crate::jabratech::common::types::Currency;
+use crate::jabratech::common::types::InstrumentKind;
+use crate::jabratech::provider_ticker::ticker::get_tickers;
+use crate::jabratech::provider_ticker::ticker::Ticker;
+
+use exports::jabratech::component_agg_ticker::agg_ticker::AggregatedTickers;
+use exports::jabratech::component_agg_ticker::agg_ticker::Guest;
+
+use serde::ser::SerializeStruct;
+use serde::ser::Serializer;
 use serde::Serialize;
 use serde_json;
-use std::vec::Vec;
+use wasi::logging::logging::log;
+use wasi::logging::logging::Level;
+use wasmcloud::messaging::consumer;
+use wasmcloud::messaging::consumer::BrokerMessage;
 
 struct AggTicker;
 
 impl Guest for AggTicker {
     fn aggregate(instrument_name: String) -> Result<AggregatedTickers, String> {
-        todo!("Implement the single aggregate method")
+        let tickers = get_ticker_data_from_provider(&instrument_name)?;
+        let agg_tickers = aggregate_tickers(instrument_name.clone(), tickers);
+
+        Ok(agg_tickers)
     }
 
     fn aggregate_and_publish(instrument_name: String) {
-        log(
-            Level::Debug,
-            "Received instrument name",
-            &instrument_name,
-        );
-        
-
-        // TODO: Figure out if we can query the list of linked exchanges via ticker-provider link
-        // TODO: Retrieve all ticker data and merge into one list
-
-        // sort the best bid and best ask list by passing the merged list to the aggregate function
-        let agg_tickers = aggregate(&instrument_name);
-
-        // publish the aggregated tickers
-        publish(instrument_name, agg_tickers);
+        match get_ticker_data_from_provider(&instrument_name) {
+            Ok(tickers) => {
+                let agg_tickers = aggregate_tickers(instrument_name.clone(), tickers);
+                publish_aggregated(instrument_name, agg_tickers)
+            }
+            Err(e) => {
+                log(Level::Error, "Failed to get ticker data from provider", &e);
+            }
+        }
     }
 }
 
-// TODO: Add merged list of tickers as input
-fn aggregate(instrument_name: &str) -> AggregatedTickers {
+type Ccy<'a> = &'a str;
+type Expiry<'a> = &'a str;
+type OptionType<'a> = &'a str;
+type Instrument<'a> = (Ccy<'a>, Expiry<'a>, u64, OptionType<'a>);
 
-    // TODO: Implement the aggregation logic
+fn parse_instrument_name<'a>(instrument_name: &'a str) -> Result<Instrument<'a>, String> {
+    let instrument_name_parts: Vec<&str> = instrument_name.split('-').collect();
 
-    // For now, we will generate random data
+    // Validate the number of parts
+    // if instrument_name_parts.len() != 4 {
+    //     return Err("Invalid instrument name format".to_string());
+    // }
+
+    let ccy = instrument_name_parts[0];
+    let expiry = instrument_name_parts[1];
+    let strike = instrument_name_parts[2]
+        .parse::<u64>()
+        .map_err(|e| format!("Failed to parse strike: {}", e))?;
+    let option_type = instrument_name_parts[3];
+
+    Ok((ccy, expiry, strike, option_type))
+}
+
+/// Fetch ticker data from the ticker provider
+fn get_ticker_data_from_provider(instrument_name: &str) -> Result<Vec<Ticker>, String> {
+    let instrument_parts = parse_instrument_name(&instrument_name)?;
+    let currency_ticker = &instrument_parts.0;
+    let kind_indicator = &instrument_parts.3;
+
+    let currency = Currency {
+        ticker: currency_ticker.to_string(),
+        name: None,
+        is_active: None,
+        decimals: None,
+        display_scale: None,
+    };
+
+    let instrument_kind = if kind_indicator.contains("C") || kind_indicator.contains("P") {
+        InstrumentKind::Opt
+    } else {
+        InstrumentKind::Spot
+    };
+
+    let tickers = get_tickers(&currency, instrument_kind).map_err(|e| e.to_string())?;
+
+    // Filter the tickers by instrument name
+    Ok(tickers
+        .into_iter()
+        .filter(|ticker| ticker.instrument_name == instrument_name)
+        .collect())
+}
+
+/// Aggregate the tickers to generate aggregated ticker data
+fn aggregate_tickers(instrument_name: String, tickers: Vec<Ticker>) -> AggregatedTickers {
+    // Aggregating best bid and ask prices and amounts
+    let mut best_bids = Vec::new();
+    let mut best_asks = Vec::new();
+
+    for ticker in &tickers {
+        best_bids.push((
+            ticker.best_bid_price,
+            ticker.best_bid_amount,
+            ticker.bid_iv,
+            ticker.index_price,
+            ticker.instrument_name.clone(),
+        ));
+        best_asks.push((
+            ticker.best_ask_price,
+            ticker.best_ask_amount,
+            ticker.ask_iv,
+            ticker.index_price,
+            ticker.instrument_name.clone(),
+        ));
+    }
+
+    // Sorting bids and asks. This sorts in descending order of price for bids and ascending order for asks
+    best_bids.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+    best_asks.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    // Taking top 5 bids and asks for the aggregation
+    let top_bids = best_bids.into_iter().take(5).collect();
+    let top_asks = best_asks.into_iter().take(5).collect();
+
     let agg_tickers = AggregatedTickers {
         instrument_name: instrument_name.to_string(),
-        strike: 68000.0,
-        kind: "Call".to_string(),
-        delta: get_random_f64(),
-        bids: vec![
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-        ],
-        asks: vec![
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-            (get_random_f64(), get_random_f64(), get_random_f64(), get_random_f64(), "Dummy".to_string()),
-        ],
-        timestamp: 0,
+        strike: tickers.first().map_or(0.0, |t| t.mark_price),
+        kind: tickers
+            .first()
+            .map_or("Unknown".to_string(), |t| t.state.clone()),
+        delta: tickers
+            .first()
+            .and_then(|t| t.greeks.as_ref())
+            .map_or(0.0, |g| g.delta.unwrap_or(0.0)),
+        bids: top_bids,
+        asks: top_asks,
+        timestamp: tickers.first().map_or(0, |t| t.timestamp),
     };
 
     agg_tickers
 }
 
-fn publish(instrument_name: String, agg_tickers: AggregatedTickers) {
+/// Publish the aggregated tickers
+fn publish_aggregated(instrument_name: String, agg_tickers: AggregatedTickers) {
     let subject = format!("agg-ticker.{}", instrument_name);
-
     let json_string = serde_json::to_string(&agg_tickers).unwrap();
-    let _ = consumer::publish(&types::BrokerMessage {
-        subject: subject,
+    let _ = consumer::publish(&BrokerMessage {
+        subject,
         reply_to: None,
         body: json_string.into(),
     });
 }
 
-fn get_random_f64() -> f64 {
-    let random_u64 = get_random_u64();
-    let random_f64 = random_u64 as f64;
-    random_f64
-}
-
-
-// Create a struct to represent the bid/ask tuple for serialization
+/// Create a struct to represent the bid/ask tuple for serialization
+#[allow(unused)]
 #[derive(Serialize)]
 struct BidAsk {
     price: f64,
@@ -92,7 +165,7 @@ struct BidAsk {
     description: String,
 }
 
-// Manually implement Serialize for AggregatedTickers
+/// Manually implement Serialize for AggregatedTickers
 impl Serialize for AggregatedTickers {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -103,25 +176,37 @@ impl Serialize for AggregatedTickers {
         state.serialize_field("strike", &self.strike)?;
         state.serialize_field("kind", &self.kind)?;
         state.serialize_field("delta", &self.delta)?;
-        
-        let bids: Vec<(String, String, String, String, String)> = self.bids.iter().map(|b| (
-            format!("{:.2}", b.0),
-            format!("{:.2}", b.1),
-            format!("{:.2}", b.2),
-            format!("{:.2}", b.3),
-            b.4.clone(),
-        )).collect();
+
+        let bids: Vec<(String, String, String, String, String)> = self
+            .bids
+            .iter()
+            .map(|b| {
+                (
+                    format!("{:.2}", b.0),
+                    format!("{:.2}", b.1),
+                    format!("{:.2}", b.2),
+                    format!("{:.2}", b.3),
+                    b.4.clone(),
+                )
+            })
+            .collect();
         state.serialize_field("bids", &bids)?;
-        
-        let asks: Vec<(String, String, String, String, String)> = self.asks.iter().map(|a| (
-            format!("{:.2}", a.0),
-            format!("{:.2}", a.1),
-            format!("{:.2}", a.2),
-            format!("{:.2}", a.3),
-            a.4.clone(),
-        )).collect();
+
+        let asks: Vec<(String, String, String, String, String)> = self
+            .asks
+            .iter()
+            .map(|a| {
+                (
+                    format!("{:.2}", a.0),
+                    format!("{:.2}", a.1),
+                    format!("{:.2}", a.2),
+                    format!("{:.2}", a.3),
+                    a.4.clone(),
+                )
+            })
+            .collect();
         state.serialize_field("asks", &asks)?;
-        
+
         state.serialize_field("timestamp", &self.timestamp)?;
         state.end()
     }
